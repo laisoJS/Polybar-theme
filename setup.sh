7341f368bcee2aded397f0de41ae4afa8e173f43
Leroy Aloïs <alo.leroy@student.helmo.be>
2024-02-11 17:36:41 +0100

    adding the autorun file

--- setup.sh
+++ setup.sh
@@ -25,3 +25,38 @@ if git clone https://github.com/laisoJS/Polybar-theme.git "$_polybar"; then
 else
     echo "An unexpected error occurred."
 fi
+
+# Define the script folder
+_script="${HOME}/.scripts"
+
+# Check if any of the standard script directories exist, otherwise, prompt to create one
+if [[ -d "${HOME}/.script" ]]; then
+    _script="${HOME}/.script"
+elif [[ -d "${HOME}/.scripts" ]]; then
+    _script="${HOME}/.scripts"
+elif [[ -d "${HOME}/.autorun" ]]; then
+    _script="${HOME}/.autorun"
+else
+    read -p "Script folder for autorun doesn't exist. Do you want to create it? (y/n): " yn
+    case $yn in
+        [Yy]* ) 
+            mkdir "$_script" ;
+            echo "Script folder for autorun created" ;
+
+            # Create the autorun script in the $_script folder
+            echo "Creating an autorun file..."
+            
+            echo "#!/bin/bash" > "${_script}/polybar-autorun.sh"
+            echo "polybar --config=${_config}/polybar/config.ini main 2>&1 | tee -a /tmp/polybar.log & disown" >> "${_script}/polybar-autorun.sh"
+            chmod +x "${_script}/autorun.sh" 
+
+            echo "Polybar autorun script successfuly created as $_script/polybar-autorun.sh"
+            ;;
+        [Nn]* ) 
+            echo "Setup aborted."; 
+            exit;;
+        * ) 
+            echo "Invalid input. Setup aborted.";
+            exit;;
+    esac
+fi
