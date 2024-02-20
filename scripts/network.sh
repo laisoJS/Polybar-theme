640e3cb8b488aaf913ed3fc10fd8506c7c66e76b
Leroy Aloïs <alo.leroy@student.helmo.be>
2024-02-20 18:11:54 +0100

    Adding docker for network module

--- scripts/network.sh
+++ scripts/network.sh
@@ -7,8 +7,8 @@ else
 fi
 
 # Defin command for cleaner code
-wifi=$(nmcli con show --active | grep -v 'lo ' | awk '{print $3}' | tail -n +2)
-vpn=$(nmcli con show --active | grep -v -e 'lo' -e 'tun0' | sed -n '2p' | awk '{print $3}')
+wifi=$(nmcli con show --active | grep -v -e 'lo' -e 'docker0' | awk '{print $3}' | tail -n +2)
+vpn=$(nmcli con show --active | grep -v -e 'lo' -e 'tun0' -e 'docker0 ' | sed -n '2p' | awk '{print $3}')
 eth=$(nmcli --fields TYPE con show --active | grep -v 'lo' | awk 'NR==2 {print $1}')
 
 # Check if the output is equal to the expected string
